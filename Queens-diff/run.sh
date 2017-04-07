0fd4b8152da2082bca8a9835f91dabdf145d857f
aqjune <aqjune@gmail.com>
2017-04-07 13:21:58 +0900

    Update run.sh

--- Queens-diff/run.sh
+++ Queens-diff/run.sh
@@ -2,15 +2,12 @@ A[0]="Queens.gcc Queens.rev.gcc"
 A[1]="Queens.rev.gcc Queens.gcc"
 
 for k in 0 1 ; do
-  for i in 1 2 3 4 5 ; do
-    for j in ${A[k]} ; do
+  echo "==================="
+  for j in ${A[k]} ; do
+    for i in 1 2 3 4 5 ; do
       echo "----- $j , $i -----"
 
-      if [ "$i" -eq 1 ]; then
-      (time ./$j) 2>/dev/null 1>/dev/null
-      else
       time ./$j > /dev/null
-      fi
     done
   done
 done
