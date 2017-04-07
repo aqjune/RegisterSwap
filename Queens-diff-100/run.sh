3976160e5ee1c846d6c53b465635dc45140c2635
Juneyoung Lee <aqjune@gmail.com>
2017-04-07 12:30:21 +0900

    Update run.sh

--- Queens-diff-100/run.sh
+++ Queens-diff-100/run.sh
@@ -1,9 +1,9 @@
-for j in Queens.gcc Queens.rev.gcc ; do
 for i in 1 2 3 4 5 6 7 ; do
+for j in Queens.gcc Queens.rev.gcc ; do
 echo "----- $j , $i -----"
 
 if [ "$i" -eq 1 ]; then
-(./$j) > /dev/null
+(time ./$j) 2>/dev/null 1>/dev/null
 else
 time ./$j > /dev/null
 fi
