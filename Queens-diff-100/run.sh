25bd0b3675602fcd1e9006498ec89103022ec174
aqjune <aqjune@gmail.com>
2017-04-07 12:33:29 +0900

    Update scripts

--- Queens-diff-100/run.sh
+++ Queens-diff-100/run.sh
@@ -1,11 +1,16 @@
-for i in 1 2 3 4 5 6 7 ; do
-for j in Queens.gcc Queens.rev.gcc ; do
-echo "----- $j , $i -----"
+A[0]="Queens.gcc Queens.rev.gcc"
+A[1]="Queens.rev.gcc Queens.gcc"
 
-if [ "$i" -eq 1 ]; then
-(time ./$j) 2>/dev/null 1>/dev/null
-else
-time ./$j > /dev/null
-fi
-done
+for k in 0 1 ; do
+  for i in 1 2 3 4 5 ; do
+    for j in ${A[k]} ; do
+      echo "----- $j , $i -----"
+
+      if [ "$i" -eq 1 ]; then
+      (time ./$j) 2>/dev/null 1>/dev/null
+      else
+      time ./$j > /dev/null
+      fi
+    done
+  done
 done
